5f7588d78af1a4d0e4744512bb64be090fd146f1
Marc Straight <marc.straight@gmail.com>
2016-01-07 23:23:24 -0500

    Updated level, particles, and generally almost everything.

---
+++ Assets/Scripts/Util/CameraTrigger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrigger : MonoBehaviour {
+
+
+	public float targetXOffset = 0;
+	public float targetYOffset = 0;
+	public float targetDistance = 0;
+
+	public float dampTime = 0.15f;
+	public bool lockCamera = false;
+	public Vector3 lockCoords;
+
+	public bool ShakeCamera = false;
+	public float shakeMagnitude = 0;
+	public float shakeDuration = 0;
+	public bool onlyShakeFirstTime = false;
+
+	private SmoothCamera2D cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main.GetComponent<SmoothCamera2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		cam.dampTime = dampTime;
+		cam.xOffset = targetXOffset;
+		cam.yOffset = targetYOffset;
+		cam.distance = targetDistance;
+		if (lockCamera) {
+			cam.LockCamera(lockCoords);
+		} else {
+			cam.UnlockCamera();
+		}
+
+		if (ShakeCamera) {
+			cam.Shake (shakeMagnitude, shakeDuration);
+			if(onlyShakeFirstTime) ShakeCamera = false;
+		}
+	}
+
+}
--- Assets/Scripts/Util/SmoothCamera2D.cs
+++ Assets/Scripts/Util/SmoothCamera2D.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SmoothCamera2D : MonoBehaviour {
 	
 	public float dampTime = 0.15f;
+	public float turnaroundDampTime = 0.5f;
 	private Vector3 velocity = Vector3.zero;
 	public Transform target;
 
@@ -14,35 +15,77 @@ public class SmoothCamera2D : MonoBehaviour {
 	public float yOffset = 0;
 	public float distance = 62f;
 
-	private Vector3 point;
+	//private Vector3 point;
 	private Vector3 delta;
 	private Vector3 destination;
+	private float xOffsetCorrected;
 
 	private bool locked = false;
 	private Vector3 lockPos;
-	
+	//Tracking scale here, so if this isn't equal to the scale of Tien's model then we're changing directions.
+	private float currentDirection; 
+	private IEnumerator runningCoroutine;
+	private float flipPercent = 1;
+
+
+	void Start(){
+		xOffsetCorrected = xOffset;
+		if(target != null)currentDirection = target.transform.localScale.x;
+	}
 
 	// Update is called once per frame
 
 	void Update () 
 	{
 		if (locked) {
-			point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			//point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 			destination = lockPos;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}else if (target)
 		{
-			point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			//point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 			delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distance)); //(new Vector3(0.5, 0.5, point.z));
 			destination = transform.position + delta;
-			destination -= new Vector3(xOffset, yOffset, 0);
+			destination -= new Vector3(xOffsetCorrected, yOffset, 0);
 			CheckBounds();
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
 		}else{
 			target = GameObject.FindGameObjectWithTag("Player").transform;
+			if(target.transform.parent != null) target = target.transform.parent;	
+			currentDirection = target.transform.localScale.x;
 		}
 
+		if (currentDirection != target.transform.localScale.x) {
+			currentDirection = target.transform.localScale.x;
+			if(runningCoroutine != null)StopCoroutine(runningCoroutine);
+			runningCoroutine = FlipCamera ();
+			StartCoroutine(runningCoroutine);
+		}
+
+
 	}
+
+	IEnumerator FlipCamera(){
+
+		float start = xOffset * currentDirection * -1;
+		float elapsed = turnaroundDampTime * (1 - flipPercent);
+		
+
+		while (elapsed < turnaroundDampTime) {
+			
+			elapsed += Time.deltaTime;          
+			
+			flipPercent = elapsed / turnaroundDampTime;         
+			xOffsetCorrected = Mathf.Lerp(start,start * -1f,flipPercent);
+			
+			yield return null;
+		}
+
+
+	}
+
+
 
 	void CheckBounds(){
 		if(destination.x > Camera_Max.x) destination.x = Camera_Max.x;
@@ -69,5 +112,36 @@ public class SmoothCamera2D : MonoBehaviour {
 
 	public void UnlockCamera(){
 		locked = false;
+	}
+
+	public void Shake(float mag, float dur){
+		StartCoroutine (ShakeCoroutine (mag, dur));
+	}
+
+	//Algorithm inspired by http://unitytipsandtricks.blogspot.com/2013/05/camera-shake.html
+	private IEnumerator ShakeCoroutine(float magnitude, float duration){
+		float elapsed = 0.0f;
+		
+		//Vector3 originalCamPos = Camera.main.transform.position;
+		
+		while (elapsed < duration) {
+			
+			elapsed += Time.deltaTime;          
+			
+			float percentComplete = elapsed / duration;         
+			float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+			
+			// map value to [-1, 1]
+			float x = Random.value * 2.0f - 1.0f;
+			float y = Random.value * 2.0f - 1.0f;
+			x *= magnitude * damper;
+			y *= magnitude * damper;
+			
+			transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+			
+			yield return null;
+		}
+		
+		//Camera.main.transform.position = originalCamPos;
 	}
 }
