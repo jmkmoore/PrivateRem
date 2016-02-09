77190e2c1cde98ab2d1fe4031b3c0c8f53e988ae
Joshua Moore <jmkmoore@gmail.com>
2016-02-08 23:52:26 -0500

    Forgot the bellfrog

---
+++ Assets/DontMoveUntilSeen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontMoveUntilSeen : MonoBehaviour {
+
+    private Animator _animator;
+    private Renderer renderer;
+    private bool isVisible;
+
+	// Use this for initialization
+	void Start () {
+        _animator = GetComponentInChildren<Animator>();
+        renderer = GetComponentInChildren<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isVisible = renderer.isVisible;
+        if (isVisible)
+        {
+            _animator.Play(Animator.StringToHash("Move"));
+        }
+    }
+}
