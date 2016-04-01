0607020449a22cbdc1f57b4923d52b7caa624516
Joshua Moore <jmkmoore@gmail.com>
2016-04-01 17:33:54 -0400

    Manually updating git branch with alpha build

---
+++ Assets/Scripts/Util/PostBossMusic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostBossMusic : MonoBehaviour {
+    private AudioSource source;
+    public AudioClip clip;
+    public GameObject myBoss;
+    bool isPlaying = false;
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myBoss == null && !isPlaying)
+        {
+            isPlaying = true;
+            source.PlayOneShot(clip);
+        }
+	
+	}
+}
